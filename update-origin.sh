70d28471829c37a36d8a20b4e96dc853ae382730
Suguru Inatomi <suguru.inatomi@gmail.com>
2018-05-11 00:43:20 +0900

    update submodule

--- update-origin.sh
+++ update-origin.sh
@@ -1,25 +1,12 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
 echo "Checking aio changes in origin..."
 
-git -C origin fetch
-localRev=$(git -C origin rev-parse master)
-remoteRev=$(git -C origin rev-parse origin/master)
+ngVersion="6.0.x"
 
-aioDiff=$(git -C origin diff master origin/master --name-only --relative aio)
+git -C origin fetch origin ${ngVersion}
+git -C origin reset FETCH_HEAD --hard
 
-if [ -n "${aioDiff}" ]; then
-    # Diffs exist
-    echo "Some changes are in aio."
-    echo -e "\n${aioDiff}\n"
-    echo "Please check diffs and sync to origin manually."
-    echo -e "\n\tgit -C origin diff master origin/master --relative aio"
-    open "https://github.com/angular/angular/compare/${localRev}...${remoteRev}"
-    exit 0
-else
-    # no diff = automatic sync
-    echo "No changes are in aio. Syncing origin automatically..."
-    git -C origin pull origin master
-fi
+node scripts/sync-origin.js
 
 echo "Finished!"

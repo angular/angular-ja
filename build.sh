d02deaa05aa8fa4ac34a346e2304556b928936c8
Suguru Inatomi <suguru.inatomi@gmail.com>
2018-11-02 09:37:33 +0900

    fix local-build

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ rsync -ar aio-ja/ .tmp/aio
 # build angular.io
 cd .tmp
 yarn install --frozen-lockfile --non-interactive
-./script/build-packages-dist.sh
+./scripts/build-packages-dist.sh
 cd aio
 yarn build-local
 

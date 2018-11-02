95cb3d7570d7839bcf7f813dc6912b3ca120e8a0
Suguru Inatomi <suguru.inatomi@gmail.com>
2018-11-02 12:15:18 +0900

    reset to stable revision

--- update-origin.sh
+++ update-origin.sh
@@ -5,7 +5,7 @@ echo "Checking aio changes in origin..."
 ngVersion="7.0.x"
 
 git -C origin fetch origin ${ngVersion}
-git -C origin reset FETCH_HEAD --hard
+git -C origin reset dc05385 --hard
 
 node scripts/sync-origin.js
 

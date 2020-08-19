dcce3478f690610af88c134efb02f2890d0cf3bb
Antonio Cardenas <antonioalx66@gmail.com>
2020-08-18 21:33:59 -0600

    support for windows users CLA Signed

---
+++ build-win.ps1
@@ -0,0 +1,22 @@
+# copy origin to temporary workspace
+Set-Location origin
+git clean -xdn
+Set-Location ..
+robocopy origin .tmp /e
+
+# overrides files from ja directory
+robocopy aio-ja/ .tmp/aio /e
+
+# build angular.io
+Set-Location .tmp
+yarn install --frozen-lockfile --non-interactive
+Set-Location aio
+yarn build
+
+Set-Location ../../
+
+# Copy robots.txt
+robocopy aio-ja/src/robots.txt .tmp/aio/dist/ /is
+
+# Modify sitemap
+((Get-Content -path .tmp/aio/dist/generated/sitemap.xml -Raw) -replace 'angular.io','angular-ja') | Set-Content -Path .tmp/aio/dist/generated/sitemap.xml

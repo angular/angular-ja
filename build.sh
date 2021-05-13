92d3107b88b746fa0024135e0165d5bac4a9048e
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-05-13 23:35:54 +0900

    build: use 4G memory

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ cd .tmp
 git apply -p1 ../scripts/git-patch/*.patch
 
 # build angular.io
-export NODE_OPTIONS="--max-old-space-size=2048"
+export NODE_OPTIONS="--max-old-space-size=4096"
 yarn install --frozen-lockfile --non-interactive
 cd aio
 yarn build

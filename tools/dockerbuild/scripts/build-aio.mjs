7c85811a1fec58038b9d4fb9f63259cd0745c963
Gérôme Grignon <gerome.grignon.lp2@gmail.com>
2022-01-31 00:37:13 +0100

    change domain name

--- tools/dockerbuild/scripts/build-aio.mjs
+++ tools/dockerbuild/scripts/build-aio.mjs
@@ -4,4 +4,4 @@ await import('./apply-localization.mjs');
 cd('/origin/aio');
 await $`yarn build`;
 await $`cp -f ../../aio-localized/src/robots.txt ./dist/`;
-await $`sed -i -e "s/angular.io/angular.jp/g" ./dist/generated/sitemap.xml`;
+await $`sed -i -e "s/angular.io/angulardocs.fr/g" ./dist/generated/sitemap.xml`;

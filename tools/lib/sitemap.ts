73c3d52a9d780072b5e5e7f549fa98a428428370
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-07-29 22:35:02 +0900

    fix: generate sitemap.xml

---
+++ tools/lib/sitemap.ts
@@ -0,0 +1,22 @@
+import { writeFile } from 'node:fs/promises';
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { glob } from './fsutils';
+import { buildOutputDir } from './workspace';
+
+export async function generateSitemap(distPath: string) {
+  const htmlFiles = await glob(['**/*.html', '!index.csr.html'], {
+    cwd: buildOutputDir,
+  });
+
+  const stream = new SitemapStream({
+    hostname: 'https://angular.jp',
+  });
+
+  for (const file of htmlFiles) {
+    stream.write({ url: file.replace(/index\.html$/, '') });
+  }
+  stream.end();
+
+  const sitemap = await streamToPromise(stream);
+  await writeFile(distPath, sitemap.toString());
+}

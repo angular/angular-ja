9d86bfd1286cee09bc3f9ac54ae4c22c93f13751
Suguru Inatomi <suguru.inatomi@gmail.com>
2025-05-31 21:38:03 +0900

    refactor: remove aio-ja files
    angular.io時代の翻訳ファイルを削除する

--- tools/lib/workspace.ts
+++ tools/lib/workspace.ts
@@ -2,6 +2,5 @@ import { resolve } from 'node:path';
 
 export const rootDir = new URL('../../', import.meta.url).pathname;
 export const adevJaDir = resolve(rootDir, 'adev-ja');
-export const aiojaDir = resolve(rootDir, 'aio-ja');
 export const buildDir = resolve(rootDir, 'build');
 export const buildOutputDir = resolve(buildDir, 'dist/bin/adev/build/browser');
